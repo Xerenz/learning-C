ef53015cff6609c3aa5eb7b2244327e373273d4a
Xerenz <martingeo15@gmail.com>
2021-04-23 10:21:41 +0530

    solution hw1.c

--- HW-Loops/hw1.c
+++ HW-Loops/hw1.c
@@ -8,3 +8,21 @@
 	2. initialise int sum = 0
 	3. for loop start -> end and sum += i
 */
+
+#include <stdio.h>
+
+int main() {
+
+	int start, end, sum = 0;
+
+	printf("Enter start and end ");
+	scanf("%d %d", &start, &end);
+
+	for (int i=start; i <= end; i++) {
+		sum += i;
+	}
+
+	printf("Sum of natural numbers from %d to %d = %d\n", start, end, sum);
+
+	return 0;
+}
