5b28325372246856133db3e96a61f00f48f99a5a
Xerenz <martingeo15@gmail.com>
2021-04-23 10:29:00 +0530

    solution hw2.c

--- HW-Loops/hw2.c
+++ HW-Loops/hw2.c
@@ -9,3 +9,26 @@
 	3. for loop 0 -> n input number sum += number
 	4. print sum and average 
 */
+
+#include <stdio.h>
+
+int main() {
+
+	int n, num, sum = 0;
+
+	printf("Enter number of terms ");
+	scanf("%d", &n);
+
+	printf("Enter numbers ");
+
+	for (int i=0; i < n; i++) {
+		scanf("%d", &num);
+		sum += num;
+	}
+
+	float average = sum / n;
+
+	printf("Sum of numbers = %d\nAverage of numbers = %f\n", sum, average);
+
+	return 0;
+}
