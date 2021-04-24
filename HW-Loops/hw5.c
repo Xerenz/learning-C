3b65ee020cef90e63ecff23d11fa98aae8c1d0f6
Xerenz <martingeo15@gmail.com>
2021-04-24 12:22:21 +0530

    session

---
+++ HW-Loops/hw5.c
@@ -0,0 +1,9 @@
+/*
+	Write a program to find all the divisors of a number
+
+	Hint: 
+
+	1. Input num
+	2. for loop 2 -> n/2
+	3. check num divisible by i
+*/
--- loops/pattern3.c
+++ loops/pattern3.c
@@ -10,7 +10,25 @@ int main() {
       * * *
         *
 
+        num stars = 2*(rows - i) + 1
+
+			
 	*/
+
+	int rows;
+
+	printf("Enter number of rows ");
+	scanf("%d", &rows);
+
+	for (int i=1; i <= rows; i++) { // i is the i th row
+		for (int k=0; k < i-1; k++) { 
+			printf(" "); // print spaces before stars
+		}
+		for (int j=1; j <= 2*(rows - i) + 1; j++) { // 2 * (rows - i) + 1 -> number of stars
+			printf("*"); // print stars
+		}
+		printf("\n");
+	}
 
 	
 	return 0;
